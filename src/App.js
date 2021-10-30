83549ed6ce23719f7e0318bfc3d2d7ab7f1302fc
forhad <dev.forhadkhan@gmail.com>
2021-10-30 22:25:14 +0600

    composition added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
+import Bracket from "./components/composition/Bracket";
+import Emoji from "./components/composition/Emoji";
+import Text from "./components/composition/Text";
+
 function App() {
-  return 'Hello';
+  return (
+    <Emoji>
+      {({ addEmoji }) => (
+        <Bracket>
+          {({ addBracket }) => (
+            <Text addEmoji={addEmoji} addBracket={addBracket} />
+          )}
+        </Bracket>
+      )}
+    </Emoji>
+  );
 }
 
 export default App;
---
+++ src/components/inheritance/Text.js
@@ -0,0 +1,13 @@
+import Emoji from "./Emoji";
+
+export default class Text extends Emoji{
+    // eslint-disable-next-line no-useless-constructor
+    constructor(props){
+        super(props);
+    }
+
+    render() {
+        const decoratedText = this.addEmoji('I am JS', '💖');
+        return super.render(decoratedText)
+    }
+}
